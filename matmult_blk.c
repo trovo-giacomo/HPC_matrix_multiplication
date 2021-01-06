f3f3076c278654c7c679940bb8fe0282ddea1a49
Giacomo <s202447@student.dtu.dk>
2021-01-06 17:18:51 +0100

    Update matmult_blk, block in the column direction

--- matmult_blk.c
+++ matmult_blk.c
@@ -1,15 +1,17 @@
 #include <math.h>
 void matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs){ 
-    int i1, i2,j,z;
-    
+    int i1, i2,j1, j2,z;
+
     for(i1 = 0; i1<m; i1+=bs){
-        for(j=0; j<n; j++){
+        for(j1=0; j1<n; j1+=bs){
             for(i2=0; i2 < fmin(m-i1,bs); i2++){
-                double sum = 0;
-                for(z=0; z<k; z++){
-                    sum += A[i1+i2][z]*B[z][j];
+                for(j2 =0; j2 < fmin(n-i2,bs); j2++){
+                    double sum = 0;
+                    for(z=0; z<k; z++){
+                        sum += A[i1+i2][z]*B[z][j1+j2];
+                    }
+                    C[i1+i2][j1+j2] = sum;
                 }
-                C[i1+i2][j] = sum;
             }
         }
     }
