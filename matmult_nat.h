9ad67e5ef0c43f80f62c3021470721ee6fd6fe35
Giacomo <s202447@student.dtu.dk>
2021-01-08 11:43:21 +0100

    Part 4 experiments + update blk

--- matmult_nat.h
+++ matmult_nat.h
@@ -1,6 +1,1 @@
-#ifndef __MATADD_H
-#define __MATADD_H
-
-void matmult_nat(int m, int n,, int k, double **A, double **B, double **C);
-
-#endif
+void matmult_nat(int m, int n, int k, double **A, double **B, double **C);
